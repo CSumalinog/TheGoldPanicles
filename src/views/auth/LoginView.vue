fa9d063245ea395d42ef97eba507b3f7532d894e
Christian <sumalinogchristianrey316@gmail.com>
2025-04-06 22:24:30 +0800

    feat: added login as container

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -2,6 +2,14 @@
 import { ref } from 'vue'
 
 const theme = ref('light')
+const selectedRole = ref(null)
+const selectedClientType = ref(null)
+
+const roles = ref([
+  { title: 'Client', value: 'client' },
+  { title: 'Staffer', value: 'staffer' },
+  { title: 'Admin', value: 'admin' },
+])
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
@@ -16,7 +24,6 @@ function onClick() {
 
         <v-btn
           :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
           slim
           @click="onClick"
         ></v-btn>
@@ -24,9 +31,48 @@ function onClick() {
 
       <v-main>
         <v-container>
-          <h1>Main Content</h1>
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <img src="" alt="Logo" class="logo" />
+              <span class="font-weight-bold ml-3">"We never flinched in serving you the Truth"</span>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <v-card class="mx-auto">
+                <template v-slot:title>
+                  <span class="d-flex justify-center text-center font-weight-bold"
+                    >Welcome, let's set you up !</span
+                  >
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-container class="d-flex flex-column align-center justify-center">
+                    <v-card width="350" class="pa-4">
+                      <v-select
+                        v-model="selectedRole"
+                        :items="roles"
+                        item-title="title"
+                        item-value="value"
+                        label="Login as"
+                        outlined
+                        class="mb-4"
+                      />
+                    </v-card>
+                  </v-container>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-container>
       </v-main>
+
+      <v-footer class="d-flex justify-center text-center" border app>
+        © 2025 - TGP | All Rights Reserved
+      </v-footer>
     </v-app>
   </v-responsive>
 </template>
